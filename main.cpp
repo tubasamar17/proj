7f9e96816ff88f3a251316c3e9d4ebe41fd605c6
Tuba Samar <tubamalik179@gmail.com>
2026-02-02 23:29:18 +0500

    message

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Function to add two numbers
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1 = 5, num2 = 3;
+    int result = add(num1, num2);
+    
+    
+    cout << "Sum of " << num1 << " and " << num2 << " is: " << result << endl;
+    cout << "Sum of " << num1 << " and " << num2 << " is: " << result << endl;
+    return 0;
+} 
